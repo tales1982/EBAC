600c50bef18b4b0c4c82e644a7f8146fd0b92721
tales1982 <142450583+TalesPortfolio@users.noreply.github.com>
2023-09-25 23:16:15 +0200

    comesando o footer

--- Front-End/a_freira/src/scripts/main.js
+++ Front-End/a_freira/src/scripts/main.js
@@ -1,7 +1,15 @@
+// Costante Share
 const dropDown = document.querySelector('.btn_hidden');
 const linkShare = document.querySelector('.hidden');
 let isHidden = true; // Variável para rastrear o estado atual
 
+//constant hamburg
+const  btnHamburguer = document.querySelector('#menuHamburg');
+const aside = document.querySelector('.divHamburg__hidden')
+const closeAside = document.querySelector('#closeAside');
+
+
+//Evento Share
 dropDown.addEventListener('click', function () {
   if (isHidden) {
     // Adicionar a classe 'hiden.fechado' quando estiver fechado
@@ -14,3 +22,12 @@ dropDown.addEventListener('click', function () {
   }
   isHidden = !isHidden; // Inverter o estado atual
 });
+
+//Evento Hamburg
+btnHamburguer.addEventListener('click',function(){
+  aside.style.display = 'block';
+})
+
+closeAside.addEventListener('click',function(){
+  aside.style.display = 'none';
+})
