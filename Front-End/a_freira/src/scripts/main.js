18f10b9bddf166f061c42e1fd06d3ca86b01bcfc
Tales Lima <102876696+tales1982@users.noreply.github.com>
2023-09-27 12:54:13 +0200

    comesandoa abri e fecha no js

--- Front-End/a_freira/src/scripts/main.js
+++ Front-End/a_freira/src/scripts/main.js
@@ -1,7 +1,7 @@
 // Div Share Start
 const btnShare = document.querySelector('#Btn-share');
 const closeShareX = document.querySelector('#closeShare');
-const divShare = document.querySelector('.hero__share'); // Seleciona a div Share
+let divShare = document.querySelector('.hero__share'); // Seleciona a div Share
 
 // Inicialmente, esconda a div Share definindo a max-height como 0
 divShare.style.maxHeight = '0px';
@@ -22,3 +22,19 @@ closeShareX.addEventListener('click', function () {
     divShare.style.maxHeight = '0px';
 });
 // Div Share End
+
+//=== Div Hamburguer start ===
+const btn_hamburg = document.querySelector("#btn-hamburg");
+let divHamburg = document.querySelector(".btnHamburguerVisivel");
+divHamburg.style.maxWidth = '0px'
+
+
+btn_hamburg.addEventListener('click',function(){
+    
+if(divHamburg.style.maxWidth == '0px'){
+    divHamburg.style.maxWidth = '300px'
+}else{
+    divHamburg.style.maxWidth = '0px'
+}
+
+})
