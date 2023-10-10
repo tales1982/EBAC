efd610e4bcebfe4fa8b2e4bbb9703c2ba0e9509a
Tales Lima <102876696+tales1982@users.noreply.github.com>
2023-10-10 16:26:10 +0200

    Polimorfismo

---
+++ Front-End/Orientacao-a-objeto/04-polimorfismo/main.js
@@ -0,0 +1,10 @@
+//============== Polimorfismo ====================
+// O polirfismo e a capacidade de executa a mesma tarefa de formas diferentes.
+
+
+//
+function Funcionario(nome, cargo, salario){
+    this.nome = nome;
+    this.cargo = cargo;
+    this.salario = salario;
+}
