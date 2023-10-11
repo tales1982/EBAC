772937af977aed527ade3d8002ea0c03a25598fc
tales1982 <142450583+TalesPortfolio@users.noreply.github.com>
2023-10-11 21:55:23 +0200

    Funcao Criando classes

--- Front-End/Orientacao-a-objeto/00_js/main.js
+++ Front-End/Orientacao-a-objeto/00_js/main.js
@@ -1,8 +1,3 @@
-// • Crie uma classe de uma abstração; 
-// • Crie pelo menos duas classes que sejam herdeiras da classe criada anteriormente; 
-// • Crie pelo menos três instâncias de objetos; 
-// • No repositório do curso crie uma branchchamada oo_jse armazene o código do exercício. 
-// • Envie o link do repositório através da plataforma.
 
 
 //Classe PAI
