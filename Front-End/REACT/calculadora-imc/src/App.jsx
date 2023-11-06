c2cfae84a4458d0c727ab5cb7eb86f98ea9d1bad
TalesPortfolio <psycho_bylly@hotmail.lu>
2023-11-06 23:45:14 +0100

    configurando o react app

---
+++ Front-End/REACT/calculadora-imc/src/App.jsx
@@ -0,0 +1,19 @@
+import styles from './App.module.css'
+import Title from './components/Title/main'
+
+
+
+
+function App() {
+
+  return (
+
+    <div className={styles.container}>
+      <Title/>
+
+    </div>
+
+  )
+}
+
+export default App
---
+++ Front-End/REACT/calculadora-imc/src/components/Title/main.jsx
@@ -0,0 +1,13 @@
+import styles from './Title.module.css';
+
+const Title = () => {
+  return (
+    <div className={styles.title}>
+           <h1 className={styles.titleH1}>Calculateur IMC - Santé</h1>
+           <h2 className={styles.titleH2}>Votre outil pour vérifier l`indice de masse corporelle.</h2>
+    </div>
+ 
+  );
+}
+
+export default Title;
