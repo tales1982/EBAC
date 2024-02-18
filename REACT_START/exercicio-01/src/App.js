b4774dd9ed4f7f6fa938e88d9799130f827b4f9a
tales1982 <102876696+tales1982@users.noreply.github.com>
2024-02-18 15:53:52 +0100

    Exercicio 1 de React Helle World

---
+++ REACT_START/exercicio-01/src/App.js
@@ -0,0 +1,46 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="Header">
+        <h1>EBAC execicios de React Modulo - 1</h1>
+      </header>
+      <section className="section-text">
+        <div class="elem1">
+          <p className="paragraphe elem10">1° Hello World</p>
+        </div>
+        <div  class="elem2">
+          <p className="paragraphe elem10">2° Hello World </p>
+        </div>
+        <div  class="elem3">
+          <p className="paragraphe elem10">3° Hello World </p>
+        </div>
+        <div  class="elem4">
+          <p className="paragraphe elem10">4° Hello World </p>
+        </div>
+        <div  class="elem5">
+          <p className="paragraphe elem10">5° Hello World </p>
+        </div>
+        <div  class="elem6">
+          <p className="paragraphe elem10">6° Hello World </p>
+        </div>
+        <div  class="elem7">
+          <p className="paragraphe elem10">7° Hello World </p>
+        </div>
+        <div  class="elem8">
+          <p className="paragraphe elem10">8° Hello World </p>
+        </div>
+        <div  class="elem9">
+          <p className="paragraphe elem10">9° Hello World </p>
+        </div>
+        <div >
+          <p className="paragraphe elem10">10° Hello World </p>
+        </div>
+      </section>
+      <footer className="Footer"><p>Copyright © 2024 | tales lima de paula </p></footer>
+    </div>
+  );
+}
+
+export default App;
